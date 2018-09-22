e0d439c2539145e4e5a0a58bf218af32e1f5d7b1
Dotun <dotunlonge@Dotuns-MBP>
2018-09-22 17:53:13 +0100

    added placeholders

--- src/json/about.json.js
+++ src/json/about.json.js
@@ -3,83 +3,83 @@ export default {
     {
       name: "Aboubakar Karim",
       title: "CEO",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Olivier Konan",
       title: "Chargé de projet, agriculture de précision",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Fabrice N’guettia",
       title: "Superviseur d’exploitations agricole",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Bakary Coulibaly",
       title: "Consultant en agronomie",
-      link: ""
+      link: "http://placehold.it/200"
     }
   ],
   technologies: [
     {
       name: "Aymard Dion",
       title: "ICT manager",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Mohamed K Coulibali",
       title: "Consultant, développeur fullstack",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Hassan Kane",
       title: "Consultant en stratégie et en robotique",
-      link: ""
+      link: "http://placehold.it/200"
     }
   ],
   admins: [
     {
       name: "Amadou Toure",
       title: "Responsable des opèrations",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Marianne Coulibaly",
       title: "Responsable administrative et financière",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Daniel Lamarche",
       title: "Consultant juridique et en développement des affaires",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Serge Assande",
       title: "Consultant en finance",
-      link: ""
+      link: "http://placehold.it/200"
     }
   ],
   drone: [
     {
       name: "Aboubakar Karim",
       title: "Pilote",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Aymard Dion",
       title: "Pilote",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Fabrice N’guettia",
       title: "Pilote",
-      link: ""
+      link: "http://placehold.it/200"
     },
     {
       name: "Olivier Konan",
       title: "Pilote",
-      link: ""
+      link: "http://placehold.it/200"
     }
   ]
 };
