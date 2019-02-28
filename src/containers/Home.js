f8958b71a7ff88c1b4cd25bd65ffc8404cb32285
Dion <dstephaneaymard@gmail.com>
2019-02-28 15:48:40 +0100

    clean gallery

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import Gallery from "./Gallery";
+
 import { withSiteData, NavLink, withRouteData, Head } from "react-static";
 import { LazyImage } from "react-lazy-images";
 
